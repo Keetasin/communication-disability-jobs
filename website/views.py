07b2160073eead3944ee6800089c2b218a3f46cf
คีตศิลป์ คงสี <keetasin01596@gmail.com>
2025-08-04 15:11:18 +0700

    Initial project setup

---
+++ website/views.py
@@ -0,0 +1,37 @@
+from flask import Blueprint, render_template
+from flask_login import login_required, current_user
+from datetime import datetime
+from flask import current_app
+from . import db
+import pytz
+from collections import Counter
+
+
+views = Blueprint('views', __name__)
+
+@views.route('/')
+def welcome():
+    return render_template('welcome.html', user=current_user)
+
+@views.route('/home')
+def home():
+    return render_template('home.html', user=current_user)
+
+
+@views.route('/account')
+@login_required
+def account():
+    user = current_user
+    email = user.email
+    first_name = user.first_name
+    current_date = datetime.now(pytz.timezone('Asia/Bangkok')).date()
+
+
+
+    return render_template(
+        'account.html', 
+        user=user, 
+        email=email, 
+        first_name=first_name, 
+        current_date=current_date, 
+    )
